9ece116eb1b138a4a273b259d1fa09b9acc44b1a
Schultzz <ms.fuge@gmail.com>
2015-05-12 13:58:03 +0200

    airlineFacade with promise added

--- server/model/airlineFacade.js
+++ server/model/airlineFacade.js
@@ -1,8 +1,14 @@
-
 var db = require('./db');
 var mongoose = require('mongoose');
+var q = require('q');
 
 var Airline = mongoose.model('Airline');
+
+function _findFlightStartDesStartDate(airportCode, date, callback) {
+
+
+
+}
 
 
 
---
+++ server/model/testDB.js
@@ -0,0 +1,57 @@
+/**
+ * Created by MS on 12-05-2015.
+ */
+var userFacade = require('./userFacade');
+
+var user = {
+    userName: "Ole",
+    email: "Ole@unicom.dk",
+    pw: "Hans"
+}
+
+//userFacade.createNewUser(user ,function (err, user) {
+//
+//    console.log("User: ",user);
+//
+//});
+
+
+userFacade.findAllUsers(function (err, data) {
+
+    console.log(data);
+
+});
+
+
+//Add airlines.
+
+//var mongoose = require('mongoose');
+//var Airline = mongoose.model("Airline");
+//
+//var newAirline = new Airline({
+//    airline: "Gruppe3",
+//    url: "http://semesterproject-testnikolai1.rhcloud.com/SemesterProjectFligths/api/flights"
+//});
+//
+//newAirline.save(function (err, airline) {
+//    if (err) {
+//        return res.end(JSON.stringify({error: err.toString()}));
+//    }
+//    else {
+//       console.log(airline);
+//    }
+//});
+//
+//var newAirline1 = new Airline({
+//    airline: "Gruppe7",
+//    url: "http://Airline7-team007.rhcloud.com/api/flight"
+//});
+//
+//newAirline1.save(function (err, airline) {
+//    if (err) {
+//        return res.end(JSON.stringify({error: err.toString()}));
+//    }
+//    else {
+//        console.log(airline);
+//    }
+//});
